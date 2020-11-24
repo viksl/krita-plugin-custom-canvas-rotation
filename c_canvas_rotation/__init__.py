6a611bc7f2a487400e681f5e4fab278259dd66db
viksl <33526847+viksl@users.noreply.github.com>
2020-11-24 16:01:51 +0100

    Update __init__.py
    More comment updates.
    Added notes for anyone wanting to adjust DISTANCE_BUFFER and/or TIMER_INTERVAL so they know what they are dealing with.

--- c_canvas_rotation/__init__.py
+++ c_canvas_rotation/__init__.py
@@ -36,6 +36,33 @@ Description:
         around the centre of the screen which means a huge movemvet across
         half or the entire screen, this plugin mitigates this as mentioned
         above.
+
+  !IMPORTANT!
+  Changes you can make manually to this plugin:
+  1. Locate the file __init__.py inside c_canvas_rotation directory.
+  2. Open in a text editor of your choice.
+  3. At the top on lines 74 and 81 you will find constants:
+      DISTANCE_BUFFER and TIMER_INTERVAL
+  4. Increase/decrease (can't be negative!) DISTANCE_BUFFER (pixels radius)
+      to grow/shrink area when the rotation is not responsive until the
+      cursor leaves this area for the first time (after that the area
+      is disabled and the rotation is allowed everywhere)
+  5.  Increase/decrease (can't be negative!) TIMER_INTERVAL (milliseconds)
+      to increase/decrease how quickly you can rotate the canvas again
+      minimum value is set to 50 milliseconds (ms) which works just fine for
+      my system but you might need to increase it.
+      Don't go to low with this as if the timer gets shorter then a tick of
+      Krita's event loop the rotation won't work (you won't experiecen any
+      error as the code works just fine but the window for rotation would
+      be soo short that it ends before you can move your cursor)
+      50 ms = 0.050 s which is a very short time so you won't notice any
+      possible "lag", even increasing to 100 ms (= 0.100 s) you will be just
+      fine, feel free to adjust to anything that works for you.
+      I suggest to keep it at 50 ms if the rotation works since increasing it
+      can eventually lead to intrusive lag-like experience (for example
+      if you set the value to 1000 ms = 1 s which means you will have to wait
+      for 1 whole second until you can rotate again which I believe doesn't
+      really make sense for anyone but I'm leaving this note here just in case)
 Copyright: (C) viksl
 """
 
