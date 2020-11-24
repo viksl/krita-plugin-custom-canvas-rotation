b4b2a299437d01d87b76f12db5db0f21067d7dc7
viksl <33526847+viksl@users.noreply.github.com>
2020-11-24 17:38:24 +0100

    Update __init__.py

--- c_canvas_rotation/__init__.py
+++ c_canvas_rotation/__init__.py
@@ -72,14 +72,14 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QTimer
 import math
 
-DISTANCE_BUFFER = 3          # THIS VALUE CAN BE CHANGED TO FIT YOUR NEEDS!
+DISTANCE_BUFFER = 3           # THIS VALUE CAN BE CHANGED TO FIT YOUR NEEDS!
                               # Units: Pixels (screen not canvas pixels)
                               # How far (in pixels) you need to move your cursor for rotation to take effect (only initially).
                               # It makes the transition from not changin rotation to changing it a bit smoother (around 0 <- initial poin)
                               # In future it might also serve as a reset for canvas reset to facilitate another function related to
                               # canvas rotation.
 
-TIMER_INTERVAL = 50           # THIS VALUE CAN BE CHANGED TO FIT YOUR NEEDS!
+TIMER_INTERVAL = 100          # THIS VALUE CAN BE CHANGED TO FIT YOUR NEEDS!
                               # Units: Miliseconds
                               # For me 50ms works as minimum value just fine.
                               # (you can go higher, if you need a lot more than this
